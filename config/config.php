c21f3275bcfab3348aa7c86ed96f30f40e75058c
Dylanbt2227 <dylantorres2227@gmail.com>
2025-08-08 20:13:58 -0600

    firts

--- config/config.php
+++ config/config.php
@@ -18,7 +18,7 @@ define('DB_USER', EnvLoader::get('DB_USER', 'root'));
 define('DB_PASS', EnvLoader::get('DB_PASS', ''));
 
 // Configuración general del sistema
-define('SITE_URL', EnvLoader::get('SITE_URL', 'http://localhost/SistemaKris'));
+define('SITE_URL', EnvLoader::get('SITE_URL', 'https://caa-i0xf.onrender.com'));
 define('SITE_NAME', EnvLoader::get('SITE_NAME', 'Sistema de Gestión Empresarial'));
 
 // Configuración de sesiones
